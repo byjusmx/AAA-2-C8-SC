60e4684c1c66720c4895fbb08dea4b34622e7a42
byjusmx <87218747+byjusmx@users.noreply.github.com>
2022-01-21 14:39:42 -0600

    Add files via upload

---
+++ Plantilla_AAA_2.py
@@ -0,0 +1,12 @@
+import turtle
+
+def draw_star():
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    for i in range():
+        turtle.fd()
+        turtle.right()
+
+draw_star(10,30,10)
+draw_star(30,60,20)
